3ef66b0656a0faff88fbfb4b204277b6c6652e94
guirRodrigues <guilherme.rodrigues@inf.ufrgs.br>
2021-11-29 11:30:39 -0300

    solution class

--- heuristic.py
+++ heuristic.py
@@ -2,4 +2,5 @@ import readDirectivesFile
 
 dict = readDirectivesFile.fileParser('directivesFile.txt')
 for element in dict:
+    print(element)
     print(dict[element])
---
+++ solution.py
@@ -0,0 +1,9 @@
+
+class Solution:
+
+    def __init__(self,diretivas):
+        self.diretivas = diretivas
+
+    def runSynthesis(self):
+        result = self.diretivas
+        self.resultados = result
