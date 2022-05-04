98ade920725c181d65a07e8bbac4507682f2a0d4
guirRodrigues <guilherme.rodrigues@inf.ufrgs.br>
2022-05-04 16:11:26 -0300

    exhaustive fix 0

--- exhaustiveSearch.py
+++ exhaustiveSearch.py
@@ -23,25 +23,31 @@ class ExhaustiveSearch(Heuristic):
         dictDir=self.parsedTxt() 
         solutionsDict = {}
         
-        final = dict.fromkeys(dictDir,None) #Cria um dicionário 'final' a partir do 'dictDir' mas 
-                                                #mantendo apenas os títulos das diretivas - seu valores são
-                                                #trocados por None
+        
         fileName = 'directives.tcl'                                 
             
+        
         solutionIndex=1
         generateScript(self.cFiles, self.prjFile)
         
         keys,values = zip(*dictDir.items())
         permutations_dicts = [dict(zip(keys, v)) for v in itertools.product(*values)]
-        index=1
+        
         for element in permutations_dicts:
                 #Progressivamente popula o dicionário 'final' e cria
             solution = Solution(element,self.cFiles,self.prjFile)         #Solutions a partir deste
             solution.runSynthesis()
+            directivesFile = open(fileName, "w")
+            for value in solution.diretivas.values():
+                if value is not None:
+                    directivesFile.write(value + '\n')
+                print(value)
+            directivesFile.close()
        
             
             deep = copy.deepcopy(solution)   
-            solutionsDict[solutionIndex] = deep               
+            solutionsDict[solutionIndex] = deep
+            print (solutionIndex)               
             solutionIndex+=1
 
         return solutionsDict
