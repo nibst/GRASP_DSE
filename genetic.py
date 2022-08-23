81b94bac8afe90b1296d88f00d75cfdabe560d1e
nikolas <nikolasps7@gmail.com>
2022-08-22 22:56:54 -0300

    gentic sem machine learnign ainda

--- genetic.py
+++ genetic.py
@@ -4,10 +4,11 @@ from pathlib import Path
 from solution import Solution
 from Script_tcl import generateScript
 import copy
-from random import randint,seed,choice
+import random
 from typing import List
 
 class GA(Heuristic):
+    _SECONDS = 43200 #12 hour
     def __init__(self,filesDict,outPath,numOfGenerations = 10):
         super().__init__(filesDict, outPath)
         self.dictDir =self.parsedTxt()
@@ -15,7 +16,11 @@ class GA(Heuristic):
         self.numOfGenes = len(self.dictDir.keys())
         self.numOfGenerations = numOfGenerations
         self.listOfKeys = list(self.dictDir.keys()) #to use numbers (indexes of list) instead of strings in the algorithm logic
+        self.crossoverRate = 0.8 #any number between 0 and 1
+        self.mutationRate = 0.1 #any number between 0 and 1
+        self.chaceToOverwrite = 0.5 #probability of overwritting parent with offspring if offspring dominates in one of the objectives
         self.createSolutionsDict()
+        
         
     def createSolutionsDict(self):
         """
@@ -52,23 +57,56 @@ class GA(Heuristic):
 
         mutation com mutacao aleatorio do domninio
         """
-        
+        """
+        maybe do something like this:
+        mutation= True
+        crossover =True
+        if random() < self.crossOverRate:
+            offspring = self.crossover(parent1,parent2)
+        else:
+            crossover = False
+        if random() < self.mutationRate:
+            offspring = self.mutation(offspring)
+        else:
+            mutation = False
+        if crossover or mutation:
+            new_population.extend([offspring])
+
+        """
+        random.seed(1)
+        start = time.time()
         population = self.randomSample() #list of random Solutions (without their HLS results yet)
         for i in range(self.numOfGenerations):
             new_population = []
             while len(new_population) < self.numOfIndividuals:
                 parent1,parent2 = self.selector(population,5)
-                offspring1,offspring2 = self.crossover(parent1,parent2)
-                offspring1 = self.mutation(parent1)
-                offspring2 = self.mutation(offspring2)
-                new_population.extend([offspring1,offspring2])
-        population = new_population
-        return self.top(2,population)
+                offspring = self.crossover(parent1,parent2)
+                offspring = self.mutation(offspring)
+                try:
+                    self.synthesisWrapper(offspring)
+                except Exception as e:
+                    print(e)
+                else:
+                    parent1,parent2 = self.overwriteParent(parent1,parent2,offspring)
+                    new_population.extend([offspring])
+                end = time.time()
+                totalTime = (end-start)
+                if totalTime >= self._SECONDS:
+                    return
+            population = new_population
 
 
     def selector(self,population,numOfIndividuals):
         #tournament
         part = self.__uniformRandom(population,numOfIndividuals)
+        for individual in part:
+            #just synthesize solutions that have not been synthesized yet
+            if None in individual.resultados.values():
+                try:
+                    self.synthesisWrapper(individual)
+                except Exception as e:
+                    print(e)
+                    
         return self.top(2,part)
 
     def __uniformRandom(self,population,numOfIndividuals):
@@ -77,7 +115,7 @@ class GA(Heuristic):
         indexes = [] # indexes of the individuals that got selected
         i = 0
         while i < numOfIndividuals:
-            individualIndex = randint(0,domainLenght-1)
+            individualIndex = random.randint(0,domainLenght-1)
             if individualIndex not in indexes:
                 indexes.append(individualIndex)
                 i+=1
@@ -86,10 +124,9 @@ class GA(Heuristic):
             part.append(population[index])
         return part
             
-    def randomSample(self,randomState = 1):
+    def randomSample(self):
         controlTree = {}
         sample = []
-        seed(randomState)
         onePermutation = None
         i=0
         while i<(self.numOfIndividuals):
@@ -104,43 +141,86 @@ class GA(Heuristic):
 
         topSolutions = [] #top "numOfTops" solutions
         for count,individual in enumerate(population):
-            try:
-                self.synthesisWrapper(individual)
-            except Exception as e:
-                print(e)
-            else:
-                if count >= numOfTops:
-                    self.__removeWorstSolution(topSolutions)
-                topSolutions.append(individual) 
+            if count >= numOfTops:
+                self.__removeWorstSolution(topSolutions)
+            topSolutions.append(individual) 
         return topSolutions
 
     def mutation(self,individual:Solution):
-        geneToMutate = randint(0,self.numOfGenes-1)
-        gene = copy.deepcopy(self.dictDir[self.listOfKeys[geneToMutate]])#copy cause we gonna modify
-        gene.remove(individual.diretivas[self.listOfKeys[geneToMutate]]) #remove current gene value from the choices
-        mutation = choice(gene)
-        individual.diretivas[self.listOfKeys[geneToMutate]] = mutation
+        
+        if random.random() < self.mutationRate:
+            geneToMutate = random.randint(0,self.numOfGenes-1)
+            gene = copy.deepcopy(self.dictDir[self.listOfKeys[geneToMutate]])#copy cause we gonna modify
+            gene.remove(individual.diretivas[self.listOfKeys[geneToMutate]]) #remove current gene value from the choices
+            mutation = random.choice(gene)
+            individual.diretivas[self.listOfKeys[geneToMutate]] = mutation
         return individual
     
     def crossover(self,parent1,parent2):
-        #TODO probably can do both offsprings in one loop
-        cutPoint = choice(self.listOfKeys)
-        offspringDirectives = {}
-        parent = parent1
-        for key in self.listOfKeys:
-            if key == cutPoint:
-                parent = parent2 #swap from which parent take genes
-            offspringDirectives[key] = parent.diretivas[key]
-        offspring = Solution(offspringDirectives, self.cFiles, self.prjFile)
+        #TODO dont know about returnming copy of parent if the crossover dont happen
         
+        if random.random() < self.crossoverRate:
+            cutPoint = random.choice(self.listOfKeys)
+            offspringDirectives = {}
+            parent = parent1
+            for key in self.listOfKeys:
+                if key == cutPoint:
+                    parent = parent2 #swap from which parent take genes
+                offspringDirectives[key] = parent.diretivas[key]
+            offspring = Solution(offspringDirectives, self.cFiles, self.prjFile)
+        else:
+            offspring = copy.deepcopy(parent1)
         return offspring
-    def overwriteParent():
+    def overwriteParent(self,parent1,parent2,offspring):
         """.
         Replace one of the parents if it dominates it in all the
         objectives (area and latency). If the offspring only dominates one of the objectives, it is randomly decided if it
         substitutes one of the parents or not.
         """    
+        #if offspring dominates parent1 and parent2 in all objectives, replace one of the parents randomly            
+        if self.dominateInBothMetrics(offspring,parent1,'resources','latency') and self.dominateInBothMetrics(offspring,parent2,'resources','latency'):
+            #replace one of the parents randomly
+            if random.random() < 0.5:
+                parent1 = offspring
+            else:
+                parent2 = offspring
+            return parent1,parent2
+        #if offspring dominates parent1 in all objectives, replace parent1
+        elif self.dominateInBothMetrics(offspring,parent1,'resources','latency'):
+            parent1 = offspring
+            return parent1,parent2
+        #if offspring dominates parent2 in all objectives, replace parent2
+        elif self.dominateInBothMetrics(offspring,parent2,'resources','latency'):
+            parent2 = offspring
+            return parent1,parent2
+
+        #if offspring dominates parent1 and parent2 in one objective, random chance to replace random parent with offspring
+        elif self.__dominateAtLeastOneMetric(offspring,parent1,'resources','latency') and self.__dominateAtLeastOneMetric(offspring,parent2,'resources','latency'):
+            chance = random.random()
+            #int total parents have 0.5 chance to be replaced
+            if chance < 0.25:
+                parent1 = offspring
+            if chance > 0.75:
+                parent2 = offspring
+            return parent1,parent2
+
+        #if offspring dominates parent1 in one objective, random chance to replace parent1 with offspring
+        if self.__dominateAtLeastOneMetric(offspring,parent1,'resources','latency'):
+            if random.random() < 0.5:
+                parent1 = offspring
+            return parent1,parent2
+
+        #if offspring dominates parent2 in one objective, random chance to replace parent1 with offspring
+        if self.__dominateAtLeastOneMetric(offspring,parent2,'resources','latency'):
+            if random.random() < 0.5:
+                parent2 = offspring
+            return parent1,parent2  
         
+        return parent1,parent2
+    
+    def __dominateAtLeastOneMetric(self,Solution1,Solution2,metric1,metric2):
+        #testa se a Solution1  domina a Solution2
+        return ((Solution2.resultados[metric1]>=Solution1.resultados[metric1]) or (Solution2.resultados[metric2] >= Solution1.resultados[metric2]))
         
     def __generateRandomPermutation(self,controlTree:dict):
         node = controlTree
@@ -148,7 +228,7 @@ class GA(Heuristic):
         isNewPermutation = False #flag para verificar se é permutacao/solucao/design repetida ou nao
         for directive in self.dictDir:              
             domainLenght = len(self.dictDir[directive])   
-            randomDirective = randint(0,domainLenght-1)
+            randomDirective = random.randint(0,domainLenght-1)
             newPermutation[directive] = self.dictDir[directive][randomDirective] 
             if randomDirective in node:
                 node = node[randomDirective]
