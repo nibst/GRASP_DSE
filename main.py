d3a72d415270bf801ae4c9fb68065bc93e4c72fa
nikolas <nikolasps7@gmail.com>
2022-12-23 03:39:57 -0300

    main atualizada

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from heuristics.impl.genetic import GA
 import argparse
 #import matplotlib.pyplot as plt
 from matplotlib.widgets import Cursor
+from utils.paretoComparer import ParetoComparer
 from utils.plotMaker import PlotMaker
 import pandas as pd
 from predictor.estimators.randomforest.randomForest import RandomForestEstimator
@@ -18,6 +19,8 @@ from predictor.preprocessing.preProcessor import PreProcessor
 from sklearn.model_selection import train_test_split
 from predictor.estimators.m5p.m5pEstimator import M5PrimeEstimator
 from predictor.estimators.m5p.m5pFactory import M5PrimeFactory
+import pickle 
+
 
 if __name__ == "__main__": 
     
@@ -51,29 +54,39 @@ if __name__ == "__main__":
     #heuristic = RandomSearchWithEstimator(filesDict, 'directives.tcl', model)
 
     factory = RandomForestFactory(filesDict["dFile"])
-    heuristic1 = GA(filesDict,'directives.tcl',factory,5)
-
-
-    model = RandomForestEstimator(filesDict['dFile'])
-
-    #heuristic2 = GRASP(filesDict,'directives.tcl',model,timeLimit=5,trainTime=1,saveInterval=1500)   
+    #heuristic1 = GA(filesDict,'directives.tcl',factory,5)
+    
+    heuristic1 = GRASP(filesDict,'./domain/directives.tcl',model,timeLimit=36000,trainTime=10800,saveInterval=1500,RCLSynthesisInterval=2)   
     #file para plotar o resultado do computador remoto, caso queira interagir com o plot ao invés de ser só um jpg
-    #heuristic2.writeToFile('./Plot/solutionsFile3')
+    heuristic1.writeToFile('./dse/gsm_GRASP10h_3h_2')
+    #heuristic1 = GA(filesDict,'./domain/directives.tcl',factory,36000,saveInterval=1500)
+    #heuristic1.writeToFile('./dse/aes_genetic10h')
 
-    #with open("./Plot/solutionsFile",'rb') as file:
-    #    heuristic1 = pickle.load(file)
-    #with open("./Plot/solutionsFile2",'rb') as file:
-    #    heuristic2 = pickle.load(file)
+
+    '''
+    with open("./dse/gsm_random14h",'rb') as file:
+        heuristic1 = pickle.load(file)
+    '''
+    with open("./dse/gsm_GRASP10h_3h",'rb') as file:
+        heuristic2 = pickle.load(file)
+    
+
+   
+
+    
+
     RESOURCE_TO_COMPARE = 'resources'
     ######################### GRAPH
-    plt = PlotMaker("sha", RESOURCE_TO_COMPARE, 'latency')
-    plt.createPlot(heuristic1.solutions) #blue
-    #plt.createPlot(heuristic2.solutions) 
-    '''
+
     comparer = ParetoComparer(RESOURCE_TO_COMPARE,'latency')
     print(comparer.compare(heuristic1,heuristic2))
     print(comparer.compare(heuristic2,heuristic1))
-    '''
+   
+    plt = PlotMaker("gsm", RESOURCE_TO_COMPARE, 'latency')
+    plt.createPlot(heuristic1.solutions) #blue
+    plt.createPlot(heuristic2.solutions) 
+    
+
 
     #   plt.createPlot(heuristic.finalPopulation)
     #paretoPlt = PlotMaker("paretos firewall", RESOURCE_TO_COMPARE, 'latency')
