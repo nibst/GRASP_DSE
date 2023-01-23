22f9fec330418152bd8e50b6842174de3bb8372e
nikolas <nikolasps7@gmail.com>
2023-01-22 23:32:23 -0300

    arrumei funcao

--- utils/timeLapsedSolutionsSaver.py
+++ utils/timeLapsedSolutionsSaver.py
@@ -1,26 +1,29 @@
+import copy
 import pickle
 import time
 from typing import List
 from domain.solution import Solution
 from heuristics.heuristic import Heuristic
+from utils.abstractSolutionsSaver import SolutionsSaver
 
-class TimeLapsedSolutionsSaver():
+class TimeLapsedSolutionsSaver(SolutionsSaver):
     def __init__(self,saveInterval=None):
         self.saveInterval = saveInterval
-        self.solutions = []
         self.numSaves = 0
         self.start = time.time()
 
     def save(self,solutions:List[Solution],savePath):
         #save all current solutions 
-        self.solutions.append(solutions)
+        
         timeElapsed = time.time() - self.start
         if self.saveInterval:
             if timeElapsed >= self.saveInterval:
-                self.__writeToFile(f'{savePath}{self.numSaves}')
+                shallow = list(solutions)  
+                self.__writeToFile(shallow,f'{savePath}{self.numSaves}')
                 self.numSaves+=1
                 self.start = time.time() # reset timer
                 
-    def __writeToFile(self,filePath):
+                
+    def __writeToFile(self,solutions,filePath):
         with open(filePath, 'wb') as solutionsFile:
-            pickle.dump(self.solutions, solutionsFile)
+            pickle.dump(solutions, solutionsFile)
