5cf3b5ab949cbd44381e54e509d497c132743fbd
nikolas <nikolasps7@gmail.com>
2023-11-06 13:51:27 -0300

    call vitis file

---
+++ domain/callVitis.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /tools/Xilinx/Vivado/Vitis_HLS/2023.1/settings64.sh
+vivado_hls -f ./domain/script.tcl
--- domain/scriptBath.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set PATH=%PATH%;C:\Xilinx\Vivado\2020.1\bin
-call C:\Xilinx\Vivado\2020.1\settings64.bat
-vivado_hls -f .\script.tcl
